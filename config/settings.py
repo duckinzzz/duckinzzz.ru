ebca8a675de9ec15cdb0fdc552328f2a022a52aa
duckinzzz <yurchenko.ps@ya.ru>
2026-02-01 16:56:32 +1000

    fix: staticfiles paths

--- config/settings.py
+++ config/settings.py
@@ -98,10 +98,6 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "frontend" / "static",
-    BASE_DIR / "crstats" / "static",
-]
 
 STATIC_ROOT = BASE_DIR / "static"
 
