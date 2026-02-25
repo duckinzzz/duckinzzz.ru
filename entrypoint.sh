930919aabd6ac3739821f3786ecb0779dd0b6534
duckinzzz <yurchenko.ps@ya.ru>
2026-02-25 23:32:41 +1000

    refactor: docker config

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput --clear
 
 echo "Starting Gunicorn..."
-exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 1
